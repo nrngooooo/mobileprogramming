d84c8db49569bd0fef7d7969dea424d80aa7e5d2
nrngooooo <is21d005@mandakh.edu.mn>
2023-02-03 14:24:57 +0800

    xcvbnm,.

--- lab1/App.js
+++ lab1/App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>hello it's my first app</Text>
+      <Image source={require('./assets/favicon.png')}
+      style={{width: 200,height: 200,}}></Image>
       <StatusBar style="auto" />
     </View>
   );
